Fix the image scaling issue

lib/model/boxes3d.py
@@ -129,4 +129,4 @@ def box_to_rgb_proj(box):
     for i in range(box.shape[0]):
         rgb[i,:] = top_to_rgb_coord(*box[i,:])
 
-    return np.hstack((rgb.min(axis=0), rgb.max(axis=0))).astype(np.int32)
+    return np.hstack((rgb.min(axis=0), rgb.max(axis=0)))

lib/nets/vgg16.py
@@ -166,7 +166,7 @@ class vgg16(Network):
       net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3],
                         trainable=self.is_training, scope='im/conv5')
       size = tf.shape(net)
-      pre_roi_pooling_net = tf.image.resize_images(net, [size[1] * 4, size[2] * 4])
+      pre_roi_pooling_net = tf.image.resize_images(net, [size[1] * 2, size[2] * 2])
       self._layers['im/conv5_3'] = pre_roi_pooling_net
       self._act_summaries.append(pre_roi_pooling_net)
 