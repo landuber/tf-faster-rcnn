Scaling variations addressed and anchor depth calculations corrected

lib/layer_utils/generate_anchors.py
@@ -11,10 +11,6 @@ from __future__ import print_function
 import numpy as np
 from model.common import *
 
-ANCHOR_DEPTH = 1.56/TOP_Z_DIVISION
-ANCHOR_CENTER =  (-0.78 - TOP_Z_MIN)/TOP_Z_DIVISION
-
-
 # Verify that we compute the same anchors as Shaoqing's matlab implementation:
 #
 #    >> load output/rpn_cachedir/faster_rcnn_VOC2007_ZF_stage1_rpn/anchors.mat
@@ -50,7 +46,7 @@ def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
   scales wrt a reference (0, 0, 15, 15) window.
   """
 
-  base_anchor = np.array([1, 1, 1, base_size, base_size, base_size]) - 1
+  base_anchor = np.array([1, 1, 1, base_size, base_size, ANCHOR_DEPTH]) - 1
   ratio_anchors = _ratio_enum(base_anchor, ratios)
   anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
                        for i in range(ratio_anchors.shape[0])])
@@ -59,16 +55,16 @@ def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
 
 def _whctrs(anchor):
   """
-  Return width, height, length, x center, y center and z center for an anchor (window).
+  Return width, height, depth, x center, y center and z center for an anchor (window).
   """
 
   w = anchor[3] - anchor[0] + 1
   h = anchor[4] - anchor[1] + 1
-  l = ANCHOR_DEPTH
+  d = anchor[5] - anchor[2] + 1
   x_ctr = anchor[0] + 0.5 * (w - 1)
   y_ctr = anchor[1] + 0.5 * (h - 1)
-  z_ctr = ANCHOR_CENTER
-  return w, h, l, x_ctr, y_ctr, z_ctr
+  z_ctr = anchor[2] + 0.5 * (d - 1)
+  return w, h, d, x_ctr, y_ctr, z_ctr
 
 
 def _mkanchors(ws, hs, ds, x_ctr, y_ctr, z_ctr):
@@ -94,12 +90,12 @@ def _ratio_enum(anchor, ratios):
   Enumerate a set of anchors for each aspect ratio wrt an anchor.
   """
 
-  w, h, l, x_ctr, y_ctr, z_ctr = _whctrs(anchor)
+  w, h, d, x_ctr, y_ctr, z_ctr = _whctrs(anchor)
   size = w * h
   size_ratios = size / ratios
   ws = np.round(np.sqrt(size_ratios))
   hs = np.round(ws * ratios)
-  ds = np.ones((ws.shape[0]), dtype=np.float32) * ANCHOR_DEPTH
+  ds = np.ones((ws.shape[0]), dtype=np.float32) * d
   anchors = _mkanchors(ws, hs, ds, x_ctr, y_ctr, z_ctr)
   return anchors
 
@@ -112,7 +108,7 @@ def _scale_enum(anchor, scales):
   w, h, d, x_ctr, y_ctr, z_ctr = _whctrs(anchor)
   ws = w * scales
   hs = h * scales
-  ds = np.ones((ws.shape[0]), dtype=np.float32) * ANCHOR_DEPTH
+  ds = np.ones((ws.shape[0]), dtype=np.float32) * d
   anchors = _mkanchors(ws, hs, ds, x_ctr, y_ctr, z_ctr)
   return anchors
 

lib/nets/network.py
@@ -31,7 +31,7 @@ from model.boxes3d import *
 
 class Network(object):
   def __init__(self, batch_size=1):
-    self._feat_stride = [4, ]
+    self._feat_stride = [2, ]
     self._feat_compress = [1. / 4., ]
     self._batch_size = batch_size
     self._predictions = {}
@@ -175,7 +175,7 @@ class Network(object):
       return tf.image.roi_pooling(bootom, rois,
                                   pooled_height=cfg.POOLING_SIZE,
                                   pooled_width=cfg.POOLING_SIZE,
-                                  spatial_scale=1. / 16.)[0]
+                                  spatial_scale=1. / 2.)[0]
 
   def _crop_pool_bv_layer(self, bottom, rois, name):
     with tf.variable_scope(name) as scope:
@@ -226,8 +226,9 @@ class Network(object):
       batch_ids = tf.squeeze(tf.slice(rois, [0, 0], [-1, 1], name="batch_id"), [1])
       # Get the normalized coordinates of bboxes
       bottom_shape = tf.shape(bottom)
-      height = (tf.to_float(bottom_shape[1]) - 1.) * np.float32(self._feat_stride[0])
-      width = (tf.to_float(bottom_shape[2]) - 1.) * np.float32(self._feat_stride[0])
+      # account for the 2x upsampling unlinke the 4x upsampling for bv and fv
+      height = (tf.to_float(bottom_shape[1]) - 1.) * np.float32(self._feat_stride[0] * 2.)
+      width = (tf.to_float(bottom_shape[2]) - 1.) * np.float32(self._feat_stride[0] * 2.)
       x1 = tf.slice(rois_img, [0, 0], [-1, 1], name="x1") / width
       y1 = tf.slice(rois_img, [0, 1], [-1, 1], name="y1") / height
       x2 = tf.slice(rois_img, [0, 2], [-1, 1], name="x2") / width
@@ -290,8 +291,8 @@ class Network(object):
 
   def _anchor_component(self):
     with tf.variable_scope('ANCHOR_' + self._tag) as scope:
-      height = tf.to_int32(tf.ceil(self._top_lidar_info[0, 0] / 4.))
-      width = tf.to_int32(tf.ceil(self._top_lidar_info[0, 1] / 4.))
+      height = tf.to_int32(tf.ceil(self._top_lidar_info[0, 0] / 2.))
+      width = tf.to_int32(tf.ceil(self._top_lidar_info[0, 1] / 2.))
       anchors, anchor_length = tf.py_func(generate_anchors_pre,
                                           [height, width,
                                            self._feat_stride, self._anchor_scales],

lib/nets/vgg16.py
@@ -97,7 +97,7 @@ class vgg16(Network):
       # change it so that the score has 2 as its channel size
       rpn_cls_score_reshape = self._reshape_layer(rpn_cls_score, 2, 'rpn_cls_score_reshape')
       rpn_cls_prob_reshape = self._softmax_layer(rpn_cls_score_reshape, "rpn_cls_prob_reshape")
-      rpn_cls_prob = self._reshape_layer(rpn_cls_prob_reshape, self._num_scales * 6, "rpn_cls_prob")
+      rpn_cls_prob = self._reshape_layer(rpn_cls_prob_reshape, self._num_scales * 3 * 2, "rpn_cls_prob")
       rpn_bbox_pred = slim.conv2d(rpn, self._num_scales * 3 * 6, [1, 1], trainable=self.is_training,
                                   weights_initializer=self._initializer,
                                   padding='VALID', activation_fn=None, scope='rpn_bbox_pred')
@@ -108,6 +108,7 @@ class vgg16(Network):
         rpn_labels = self._anchor_target_layer(rpn_cls_score, "anchor")
         # Try to have a determinestic order for the computing graph, for reproducibility
         with tf.control_dependencies([rpn_labels]):
+          # Sample rois from above into foreground/background percentage split using gt_boxes
           rois, _ = self._proposal_target_layer(rois, roi_scores, "rpn_rois")
       else:
         if cfg.TEST.MODE == 'nms':