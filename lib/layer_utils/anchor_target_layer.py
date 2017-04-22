6bc7f9d56c2605e9af3243c1de13e6a4672d4e69
LANDUBER KASSA <landuber@gmail.com>
2017-04-22 13:56:03 -0700

    adding rois display items

--- lib/layer_utils/anchor_target_layer.py
+++ lib/layer_utils/anchor_target_layer.py
@@ -34,8 +34,10 @@ def anchor_target_layer(rpn_cls_score, gt_boxes, im_info, _feat_stride, all_anch
   inds_inside = np.where(
     (all_anchors[:, 0] >= -_allowed_border) &
     (all_anchors[:, 1] >= -_allowed_border) &
+    (all_anchors[:, 2] >= -_allowed_border) &
     (all_anchors[:, 3] < im_info[1] + _allowed_border) &  # width
-    (all_anchors[:, 4] < im_info[0] + _allowed_border)  # height
+    (all_anchors[:, 4] < im_info[0] + _allowed_border) &  # height
+    (all_anchors[:, 5] < im_info[2] + _allowed_border)   # depth
   )[0]
 
   # keep only inside anchors
--- lib/layer_utils/proposal_layer.py
+++ lib/layer_utils/proposal_layer.py
@@ -30,7 +30,7 @@ def proposal_layer(rpn_cls_prob, rpn_bbox_pred, im_info, cfg_key, _feat_stride, 
   rpn_bbox_pred = rpn_bbox_pred.reshape((-1, 6))
   scores = scores.reshape((-1, 1))
   proposals = bbox_transform_inv(anchors, rpn_bbox_pred)
-  proposals = clip_boxes(proposals, im_info[:2])
+  proposals = clip_boxes(proposals, im_info)
 
   # Pick the top region proposals
   order = scores.ravel().argsort()[::-1]
