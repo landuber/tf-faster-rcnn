f768afab0906baba84b7cc69a8c2a3a25f4330ad
LANDUBER KASSA <landuber@gmail.com>
2017-04-21 23:29:18 -0700

    debug items

--- lib/layer_utils/generate_anchors.py
+++ lib/layer_utils/generate_anchors.py
@@ -46,11 +46,15 @@ def generate_anchors(base_size=1, ratios=[0.5, 1, 2],
   scales wrt a reference (0, 0, 15, 15) window.
   """
 
-  base_anchor = np.array([1, 1, 1, base_size, base_size, ANCHOR_DEPTH]) - 1
-  ratio_anchors = _ratio_enum(base_anchor, ratios)
-  anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
-                       for i in range(ratio_anchors.shape[0])])
-  print(anchors)
+  ws = np.array([16, 39,  6, 16, 16, 39,  6, 16, 16])
+  hs = np.array([39, 16, 16,  6, 39, 16, 16,  6, 39])
+  ds = np.ones((ws.shape[0]), dtype=np.float32) * ANCHOR_DEPTH
+  anchors = _mkanchors(ws, hs, ds, 0., 0., 0.)
+
+  #base_anchor = np.array([1, 1, 1, base_size, base_size, ANCHOR_DEPTH]) - 1
+  #ratio_anchors = _ratio_enum(base_anchor, ratios)
+  #anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
+  #                     for i in range(ratio_anchors.shape[0])])
   return anchors
 
 
