6a3e62a4c566ab80d3765e86c5562d1b92496465
LANDUBER KASSA <landuber@gmail.com>
2017-04-21 21:56:03 -0700

    debug items

--- lib/layer_utils/generate_anchors.py
+++ lib/layer_utils/generate_anchors.py
@@ -39,22 +39,15 @@ from model.common import *
 #       [-167., -343.,  184.,  360.]])
 
 #todo: determine the appropriate base_size from the paper
-def generate_anchors(base_size=6, ratios=[0.5, 1, 2],
+def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
                      scales=2 ** np.arange(0, 3)):
   """
   Generate anchor (reference) windows by enumerating aspect ratios X
   scales wrt a reference (0, 0, 15, 15) window.
   """
-
-  print('scales')
-  print(scales)
-
-  base_anchor = np.array([1, 1, 1, base_size, base_size, ANCHOR_DEPTH]) - 1
   ratio_anchors = _ratio_enum(base_anchor, ratios)
   anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
                        for i in range(ratio_anchors.shape[0])])
-  print('anchors')
-  print(anchors)
   return anchors
 
 
@@ -88,6 +81,7 @@ def _mkanchors(ws, hs, ds, x_ctr, y_ctr, z_ctr):
                        y_ctr + 0.5 * (hs - 1),
                        z_ctr + 0.5 * (ds -1)))
   return anchors
+
 
 
 def _ratio_enum(anchor, ratios):
