1ffd76b050641063db97af29295f51fbe66cef92
LANDUBER KASSA <landuber@gmail.com>
2017-04-22 19:35:17 -0700

    adding rois display items

--- lib/layer_utils/proposal_target_layer.py
+++ lib/layer_utils/proposal_target_layer.py
@@ -119,10 +119,10 @@ def _sample_rois(all_rois, all_scores, gt_boxes, fg_rois_per_image, rois_per_ima
 
   print('ones')
   print(np.sum(labels == 1))
-  #print('foregrounds: max_overlap >= fg_thresh')
-  #print(fg_inds.size)
-  #print('background: max_overlap < bg_thresh_hi & >= bg_thres_lo')
-  #print(bg_inds.size)
+  print('foregrounds: max_overlap >= fg_thresh')
+  print(fg_inds.size)
+  print('background: max_overlap < bg_thresh_hi & >= bg_thres_lo')
+  print(bg_inds.size)
 
 
   # Small modification to the original version where we ensure a fixed number of regions are sampled
