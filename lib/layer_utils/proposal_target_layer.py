d2bc499fe431797f1210f101414fa3511b7280c4
LANDUBER KASSA <landuber@gmail.com>
2017-04-22 19:44:54 -0700

    adding rois display items

--- lib/layer_utils/proposal_target_layer.py
+++ lib/layer_utils/proposal_target_layer.py
@@ -118,7 +118,7 @@ def _sample_rois(all_rois, all_scores, gt_boxes, fg_rois_per_image, rois_per_ima
                      (max_overlaps >= cfg.TRAIN.BG_THRESH_LO))[0]
 
   print('top 4')
-  print(np.argpartition(max_overlaps, -4)[-4:])
+  print(max_overlaps[np.argpartition(max_overlaps, -4)[-4:]])
   print('foregrounds: max_overlap >= fg_thresh')
   print(fg_inds.size)
   print('background: max_overlap < bg_thresh_hi & >= bg_thres_lo')
