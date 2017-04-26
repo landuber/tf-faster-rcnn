e9924ab2ff653bc975e34097db034dda48c6bb68
LANDUBER KASSA <landuber@gmail.com>
2017-04-26 16:15:07 -0700

    Adding Drop path and Auxilary

--- lib/model/boxes3d.py
+++ lib/model/boxes3d.py
@@ -77,6 +77,7 @@ def img_projection_layer(rois, image_info):
       for idx in range(rois.shape[0]):
           box = box_from_corners(rois[idx, :])
           img_rois[idx, :] = box_to_rgb_proj(box)
+      img_rois = np.where(img_rois[0] >=0 & img_rois[2] <= (image_info[0, 0] - 1) & imag_rois[1] >= 0 & imag_rois[3] <= (image_info[0, 1] - 1))
       return img_rois * image_info[0, 2]
 
 
