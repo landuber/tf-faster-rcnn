fc3dd627a251e066046f62c3eb56728090f53fe5
LANDUBER KASSA <landuber@gmail.com>
2017-05-03 14:26:20 -0700

    debug prints

--- lib/model/bbox_transform.py
+++ lib/model/bbox_transform.py
@@ -41,11 +41,15 @@ def bbox_transform(ex_rois, gt_rois):
 
 def corner_transform(rois_corners, gt_corners):
     print('rois')
-    print(rois_corners[0:10, :, :])
+    print(rois_corners[:2, :, :])
     print('gt_corners')
-    print(gt_corners[0:10, :, :])
+    print(gt_corners[:2, :, :])
     deltas = rois_corners.max(axis=1) - rois_corners.min(axis=1)
     rois_mins = rois_corners.min(axis=1)[:, np.newaxis, :]
+    print('deltas')
+    print(deltas[:2,:])
+    print('mins')
+    print(rois_mins[:2, :])
     diagonals = np.hypot(np.hypot(deltas[:,0], deltas[:,1]), deltas[:,2])
     diagonals = diagonals[:, np.newaxis]
     targets = (gt_corners - rois_mins)
